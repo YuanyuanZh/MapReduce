7ce85163a144c670db1da2776581957b51984755
Yuanyuan Zhang <yzhang171@dons.usfca.edu>
2015-10-16 22:10:35 -0700

    add file split

--- mr_seq.py
+++ mr_seq.py
@@ -57,33 +57,28 @@ class Engine(object):
         self.result_list = None
         self.output_base = output_base
 
-    # def read_by_size(self,file_object):
-    #     while True:
-    #         data = file_object.read(self.split_size)
-    #         if not data:
-    #             break
-    #         yield data
-    #
-    # def map_data(self,mapper,input_data):
-    #     for i, v in enumerate(input_data):
-    #         mapper.map(i, v)
     def create_class(self,class_name):
         if class_name == 'WordCountMap':
             return WordCountMap()
         if class_name == 'WordCountReduce':
             return WordCountReduce()
 
-    def split_file(self):
-        offset = 0
-        
+    def adjust_offset(self,file_object,start,end):
+        newpos = start
+        file_object.seek(start,0)
+        s = int(self.split_size)
+        while newpos < end and newpos < s-1 + start:
+            file_object.readline()
+            newpos = file_object.tell()
+        return newpos
 
     def execute(self):
         # Map phase
         mapper = self.create_class(self.map_class)
-        file_object = open(self.input_file)
-        input = file_object.readlines()
-        for i, v in enumerate(input):
-            mapper.map(i, v)
+        input = self.get_input()
+
+        for j, v in enumerate(input):
+                mapper.map(j, v)
 
         # Sort intermediate keys
         table = mapper.get_table()
@@ -99,6 +94,27 @@ class Engine(object):
         out_file = open(self.output_base,'w')
         value = str(self.result_list)
         out_file.write(value)
+
+    def get_input(self):
+        file_object = open(self.input_file)
+        file_object.seek(0, 2)
+        end = file_object.tell()
+        pos_list = []
+        offset = 0
+        while offset < end:
+            pos_list.append(offset)
+            offset = self.adjust_offset(file_object, offset, end)
+        input = []
+        for i in range(len(pos_list)):
+            file_object.seek(pos_list[i])
+            if i + 1 < len(pos_list):
+                size = pos_list[i + 1] - pos_list[i]
+                vi = file_object.read(size)
+            else:
+                vi = file_object.read(end - pos_list[i])
+            input.append(vi)
+        return input
+
 
 if __name__ == '__main__':
     map_name = sys.argv[1]
