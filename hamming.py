9c6ca2d7e082a35bb6b53d95bbb1bcfee8aa5f5f
Yuanyuan Zhang <yzhang171@dons.usfca.edu>
2015-10-17 22:59:15 -0700

    refactor split and add partion func

---
+++ hamming.py
@@ -0,0 +1,121 @@
+import sys
+import string
+
+class Hamming(object):
+    def get_hamming_code(self,binary_str):
+        binary_list = list(binary_str)
+        for i in range(4):
+            binary_list.insert(2**i-1,0)
+            sum =0
+        for j in [0,2,4,6,8,10]:
+            if binary_list[j] == '1':
+                sum +=1
+        binary_list[0] = str(sum%2)
+        sum = 0
+        for j in [1,2,5,6,9,10]:
+            if binary_list[j] == '1':
+                sum += 1
+        binary_list[1] = str(sum%2)
+        sum = 0;
+        for j in [3,4,5,6,11]:
+            if binary_list[j] == '1':
+                sum +=1
+        binary_list[3] = str(sum%2)
+        sum = 0;
+        for j in [7,8,9,10,11]:
+            if binary_list[j] == '1':
+                sum +=1
+        binary_list[7] = str(sum%2)
+        b_str = ''.join(binary_list)
+        return b_str
+
+    def get_ascii(self,byte_str):
+        binary_list = list(byte_str)
+        for i in [7,3,1,0]:
+            binary_list = binary_list[:i]+binary_list[i+1:]
+        b_str = ''.join(binary_list)
+        value = int(b_str,2)
+        return chr(value)
+
+class HammingEncoder(Hamming):
+    def encode(self,text):
+        hamming = ''
+        for c in text:
+            binary_str = "{0:08b}".format(ord(c))
+            hamming += self.get_hamming_code(binary_str)
+        return hamming
+
+class HammingDecoder(Hamming):
+    def decode(self, text):
+        """Return decoded text as a string."""
+        out_string = ''
+        while len(text) > 0:
+            if text[0] == '\n':
+                break
+            byte_str = text[0:12]
+            text = text[12:]
+            out_string += self.get_ascii(byte_str)
+        return out_string
+
+class HammingChecker(Hamming):
+    def check(self, text):
+        pos =0
+        positions = []
+        while(len(text)>0):
+            if text[0] == '\n':
+                break
+            byte_str = text[0:12]
+            text = text[12:]
+            c = self.get_ascii(byte_str)
+            binary_str = "{0:08b}".format(ord(c))
+            p = self.get_hamming_code(binary_str)
+            for i in [0,1,3,7]:
+                if p[i] != byte_str[i]:
+                    positions += [pos]
+        	    break
+            pos += 1
+        return positions
+
+class HammingFixer(Hamming):
+    def fix(self,text):
+        fix_text = ''
+        while(len(text)>0):
+            if text[0] == '\n':
+                break
+            byte_str = text[0:12]
+            text = text[12:]
+            #temp = byte_str[0:12]
+            c = self.get_ascii(byte_str)
+            binary_str = "{0:08b}".format(ord(c))
+            p = self.get_hamming_code(binary_str)
+            pos = 0
+            b =0
+            for i in [0,1,3,7]:
+                if p[i] != byte_str[i]:
+                    b =1
+                    pos += i+1
+            if b==1:
+        	byte_list = list(byte_str)
+        	if byte_list[pos-1] == '0':
+        	    byte_list[pos-1] = '1'
+        	else:
+        	    byte_list[pos-1] = '0'
+        	t = ''.join(byte_list)
+        	fix_text += t
+            else:
+                fix_text += byte_str
+        return fix_text
+
+class HammingError(Hamming):
+    def createError(self,pos,text):
+        byte_pos = pos/12
+        bit_pos = pos%12
+        target_str = text[byte_pos * 12:(byte_pos+1)*12]
+        target_list = list(target_str)
+        if target_list[bit_pos-1] =='1':
+            target_list[bit_pos-1] ='0'
+        else:
+            target_list[bit_pos-1] ='1'
+        error_str = ''.join(target_list)
+        rst = text[:byte_pos * 12]+error_str+text[(byte_pos+1)*12:]
+        return rst
--- mr_seq.py
+++ mr_seq.py
@@ -1,82 +1,92 @@
 import sys
-
-class Map(object):
-
-    def __init__(self):
-        self.table = {}
-
-    def map(self, k, v):
-        pass
-
-    def emit(self, k, v):
-        if k in self.table:
-            self.table[k].append(v)
-        else:
-            self.table[k] = [v]
-
-    def get_table(self):
-        return self.table
-
-
-class Reduce(object):
-
-    def __init__(self):
-        self.result_list = []
-
-    def reduce(self, k, vlist):
-       pass
-
-    def emit(self, v):
-        self.result_list.append(v)
-
-    def get_result_list(self):
-        return self.result_list
-
-class WordCountMap(Map):
-
-    def map(self, k, v):
-        words = v.split()
-        for w in words:
-            self.emit(w, '1')
-
-class WordCountReduce(Reduce):
-
-    def reduce(self, k, vlist):
-        count = 0
-        for v in vlist:
-            count = count + int(v)
-        self.emit(k + ':' + str(count))
+import mr_classes
 
 class Engine(object):
 
-    def __init__(self, input_file,split_size, map_name, reducer_name,output_base):
+    def __init__(self, input_file, split_size, class_name, num_reducers,output_base):
         self.input_file = input_file
-        self.split_size = split_size
-        self.map_class = map_name
-        self.reduce_class = reducer_name
+        self.split_size = int(split_size)
+        self.class_name = class_name
+        self.num_reducer = int(num_reducers)
         self.result_list = None
         self.output_base = output_base
 
-    def create_class(self,class_name):
-        if class_name == 'WordCountMap':
-            return WordCountMap()
-        if class_name == 'WordCountReduce':
-            return WordCountReduce()
+    def create_map_instance(self,class_name):
+        if class_name == 'WordCount':
+            return mr_classes.WordCountMap()
+        elif class_name == "hammingEnc":
+            return mr_classes.hammingEncMap()
+        elif class_name == "hammingDec":
+            return mr_classes.hammingDecMap()
+        elif class_name == "hammingFix":
+            return mr_classes.hammingFixMap()
+
+    def create_reduce_instance(self,class_name):
+        if class_name == 'WordCount':
+            return mr_classes.WordCountReduce()
+        else:
+            return mr_classes.hammingReduce()
 
     def adjust_offset(self,file_object,start,end):
+
         newpos = start
         file_object.seek(start,0)
-        s = int(self.split_size)
-        while newpos < end and newpos < s-1 + start:
+        while newpos < end and newpos < self.split_size-1 + start:
             file_object.readline()
             newpos = file_object.tell()
         return newpos
 
+    def generate_split_info(self,file_object):
+
+        file_object.seek(0, 2)
+        end = file_object.tell()
+        split_map = {}
+        offset = 0
+        if self.class_name == "hammingEnc":
+            while offset < end:
+                split_map[offset] = self.split_size
+                offset = offset+ self.split_size
+        elif self.class_name == "hammingDec":
+            while (self.split_size *8)%12 != 0:
+                self.split_size = self.split_size + 1
+            while offset <end:
+                prev = offset
+                offset = self.adjust_offset(file_object, offset, end)
+                split_map[prev] = offset - prev
+        else:
+            while offset < end:
+                prev = offset
+                offset = self.adjust_offset(file_object, offset, end)
+                split_map[prev] = offset - prev
+        return split_map
+
+    def read_input(self,file_object,split_dict):
+        input = []
+        for offset in split_dict.keys():
+            file_object.seek(offset)
+            content = file_object.read(split_dict[offset])
+            input.append(content)
+        return input
+
+    def partion(self, keys, table):
+        job_for_reduces = {}
+        num_reducer = self.num_reducer
+        num_map_result = len(keys)
+        for i in range(num_map_result):
+            hashkey = i % num_reducer
+            if hashkey in job_for_reduces:
+                job_for_reduces[hashkey][keys[i]] = table[keys[i]]
+            else:
+                job_for_reduces[hashkey] = {keys[i]: table[keys[i]]}
+
+        return job_for_reduces
+
     def execute(self):
         # Map phase
-        mapper = self.create_class(self.map_class)
-        input = self.get_input()
-
+        mapper = self.create_map_instance(self.class_name)
+        file_object = open(self.input_file)
+        split_hashmap = self.generate_split_info(file_object)
+        input = self.read_input(file_object,split_hashmap)
         for j, v in enumerate(input):
                 mapper.map(j, v)
 
@@ -85,42 +95,32 @@ class Engine(object):
         keys = table.keys()
         keys.sort()
 
-        # Reduce phase
-        reducer = self.create_class(self.reduce_class)
-        for k in keys:
-            reducer.reduce(k, table[k])
-        self.result_list = reducer.get_result_list()
+        #partion
+        job_for_reduces= self.partion(keys, table)
 
-        out_file = open(self.output_base,'w')
-        value = str(self.result_list)
-        out_file.write(value)
+        #Reduce phase
+        for i in range(len(job_for_reduces)):
+            reducer = self.create_reduce_instance(self.class_name)
+            keys = job_for_reduces[i].keys()
+            keys.sort()
+            for k in keys:
+                reducer.reduce(k, job_for_reduces[i][k])
+            self.result_list = reducer.get_result_list()
 
-    def get_input(self):
-        file_object = open(self.input_file)
-        file_object.seek(0, 2)
-        end = file_object.tell()
-        pos_list = []
-        offset = 0
-        while offset < end:
-            pos_list.append(offset)
-            offset = self.adjust_offset(file_object, offset, end)
-        input = []
-        for i in range(len(pos_list)):
-            file_object.seek(pos_list[i])
-            if i + 1 < len(pos_list):
-                size = pos_list[i + 1] - pos_list[i]
-                vi = file_object.read(size)
+            out_file = open(self.output_base+"_"+str(i),'w')
+            out = ''
+            if self.class_name == "WordCount":
+                out = str(self.result_list)
             else:
-                vi = file_object.read(end - pos_list[i])
-            input.append(vi)
-        return input
-
+                for i in self.result_list:
+                    out += str(i)
+            out_file.write(out)
 
 if __name__ == '__main__':
-    map_name = sys.argv[1]
+    class_name = sys.argv[1]
     split_size = sys.argv[2]
-    reducer_name = sys.argv[3]
+    num_reducer = sys.argv[3]
     in_filename = sys.argv[4]
     output_base = sys.argv[5]
-    engine = Engine(in_filename, split_size,map_name, reducer_name,output_base)
+    engine = Engine(in_filename, split_size,class_name, num_reducer, output_base)
     engine.execute()
