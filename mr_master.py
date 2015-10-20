24ee06ab93df98d5614b705cd4fbc3898d9e900f
jenniew <jenniewang123@gmail.com>
2015-10-19 18:12:21 -0700

    update reducer

--- mr_master.py
+++ mr_master.py
@@ -18,11 +18,12 @@ class Job():
         # create empty task list
         self.map_task_list = {}
         for i in range(len(self.splits)):
-            task = MapTask(None, self.className, None, self.splits[i], self.infile, None, self.num_reducers)
+            task = MapTask(self.jobId, i, None, self.className, None, self.splits[i], self.infile, None,
+                           self.num_reducers)
             self.map_task_list[i] = task
         self.reduce_task_list = {}
         for i in range(self.num_reducers):
-            task = ReduceTask(None, self.className, None, None, self.outfile, len(self.splits))
+            task = ReduceTask(self.jobId, i, None, self.className, None, None, self.outfile, len(self.splits))
             self.reduce_task_list[i] = task
 
     def setStatus(self, status):
@@ -33,8 +34,9 @@ class Job():
 
 
 class Task():
-    def __init__(self, task_id, className, worker):
+    def __init__(self, job_id, task_id, className, worker):
         self.task_id = task_id
+        self.job_id = job_id
         self.className = className
         self.worker = worker
         self.state = "NOT_ASSIGNED"
@@ -42,9 +44,9 @@ class Task():
 
 
 class MapTask(Task):
-    def __init__(self, task_id, className, worker, splits, infile, partitions, num_reducers):
-        Task.__init__(self, task_id, className, worker)
-        # self.split_id = split_id
+    def __init__(self, job_id, split_id, task_id, className, worker, splits, infile, partitions, num_reducers):
+        Task.__init__(self, job_id, task_id, className, worker)
+        self.split_id = split_id
         self.splits = splits
         self.infile = infile
         self.partitions = partitions
@@ -52,11 +54,12 @@ class MapTask(Task):
 
 
 class ReduceTask(Task):
-    def __init__(self, task_id, className, worker, partitions, outfile, num_mappers):
-        Task.__init__(self, task_id, className, worker)
-        # self.partition_id = partition_id
+    def __init__(self, job_id, partition_id, task_id, className, worker, partitions, outfile, num_mappers):
+        Task.__init__(self, job_id, task_id, className, worker)
+        self.partition_id = partition_id
         self.outfile = outfile
-        self.partitions = partitions
+        if partitions == None:
+            self.partitions = {}
         self.num_mappers = num_mappers
 
 
@@ -68,6 +71,7 @@ class Master():
         self.worker_id = -1
         self.job_id = -1
         self.jobs = Queue()
+        self.processing_jobs = {}
         self.task_id = -1
         self.task_list = []
 
@@ -106,60 +110,56 @@ class Master():
                 return self.worker_list[i]
         return None
 
+    def getMapResultLocation(self, job_id):
+        locations = {}
+        # find corresponding map result
+        job = self.processing_jobs[job_id]
+        map_list = job.map_task_list
+        for i in range(len(map_list)):
+            task = map_list[i]
+            if task.state == 'Finish':
+                locations[task.split_id] = task.worker
+        return locations
+
+    def assignTask(self, type, task_list):
+        for i in range(len(task_list)):
+            if task_list[i].state == 'NOT_ASSIGNED':
+                if type == 'Map':
+                    worker = self.getMapSlot()
+                else:
+                    worker = self.getReduceSlot()
+                if worker != None:
+                    task = task_list[i]
+                    self.task_id += 1
+                    task.task_id = self.task_id
+                    task.worker = worker
+                    # start task
+                    client = zerorpc.Client()
+                    client.connect(worker["address"])
+                    if type == 'Map':
+                        ret = client.startMap(task)
+                    else:
+                        ret = client.startReduce(task)
+                    if ret == 0:
+                        task.state == 'STARTING'
+                    else:
+                        task.state = 'NOT_ASSIGNED'
+
     def jobScheduler(self):
         while not self.jobs.empty():
             job = self.jobs.get()
-            num_map_tasks = 0
-            num_reducer_tasks = 0
-            for i in range(len(self.worker_list)):
-                if self.worker_list[i]['mapper'] == None:
-                    num_map_tasks += 1
-                if self.worker_list[i]['reducer'] == None:
-                    num_reducer_tasks += 1
+            self.processing_jobs[job.jobId] = job
+            # num_map_tasks = 0
+            # num_reducer_tasks = 0
+            # for i in range(len(self.worker_list)):
+            #     if self.worker_list[i]['mapper'] == None:
+            #         num_map_tasks += 1
+            #     if self.worker_list[i]['reducer'] == None:
+            #         num_reducer_tasks += 1
             partitions = []
-            # create map tasks
-            for i in range(job.map_task_list):
-                if job.map_task_list[i].state == 'NOT_ASSIGNED':
-                    worker = self.getMapSlot()
-                    if worker != None:
-                        task = job.map_task_list[i]
-                        self.task_id += 1
-                        task.task_id = self.task_id
-                        task.worker = worker
-                        #start task
-                        client = zerorpc.Client()
-                        client.connect(worker["address"])
-                        client.startMap(task)
-                        task.state == 'ASSIGNED'
-
-
-
-            for i in range(num_map_tasks):
-                self.task_id += 1
-                task = job.map_task_list[i]
-                task.task_id = self.task_id
-                task.worker = self.worker_list[i]
-            # create reducer tasks
-            for i in range(job.num_reducers):
-                self.task_id += 1
-                task = job.reduce_task_list[i]
-                task.task_id = self.task_id
-                task.worker = self.worker_list[i]
-            # submit task
-            for i in range(len(job.map_task_list)):
-                client = zerorpc.Client()
-                client.connect((job.map_task_list[i]).worker["address"])
-                client.startMap(job.map_task_list[i])
-            for i in range(len(job.reduce_task_list)):
-                client = zerorpc.Client()
-                client.connect((job.reduce_task_list[i]).worker["address"])
-                client.startReduce(job.reduce_task_list[i])
-
-
-                # splits_num = job.splits/num_map_tasks
-
-
-                # if(len(job.splits) % num_map_tasks == 0 ):
+            # assign tasks
+            self.assignTask('Map', job.map_task_list)
+            self.assignTask('Reduce', job.map_task_list)
 
     def heartBeat(self):
         return
--- mr_worker.py
+++ mr_worker.py
@@ -4,6 +4,7 @@ import socket
 import mr_master
 import mr_classes
 import input_split
+import sys
 
 
 class Worker():
@@ -15,12 +16,19 @@ class Worker():
         else:
             self.worker_address = worker_address
             self.is_remote = True
-        self.map_task_list = []
-        self.reduce_task_list = []
+        self.all_map_task_list = {}
+        self.all_reduce_task_list = {}
 
     def mapper(self, map_task):
         task = map_task
-        self.map_task_list.append(task)
+        # No this job, create it
+        if self.all_map_task_list.has_key(task.job_id) == False:
+            map_list = {}
+            map_list[task.split_id]=task
+            self.all_map_task_list[task.job_id] = map_list
+        else:
+            self.all_map_task_list[task.job_id][task.split_id] = task
+
         if task.className == 'WordCount':
             mapper = mr_classes.WordCountMap()
         if task.className == 'Sort':
@@ -34,7 +42,7 @@ class Worker():
         task.state = 'STARTING'
         task.progress = 'Starting.'
         # Map phase
-        file_object = open(task.input)
+        file_object = open(task.infile)
         input = self.read_input(file_object, task.splits)
         i = 0
         for j, v in enumerate(input):
@@ -55,26 +63,60 @@ class Worker():
 
         # partion
         partitions = mapper.partition(keys, task.num_reducers)
-        task.output = partitions
+        task.partitions = partitions
+        task.state = 'Finish'
         task.progress = 'Finish mapping.'
+
+    def collectAllInputsForReducer(self, task):
+        #get all input from other workers
+        while True:
+            client1 = zerorpc.Client()
+            client1.connect(self.master_address)
+            #get input locations from master
+            locations = client1.getMapResultLocation(task.job_id)
+            for split_id,worker in locations.items():
+                #if didn't has that input, get input from mapper; else ignore;
+                if task.partitions.has_key(split_id) == False:
+                    client = zerorpc.Client()
+                    client.connect(worker['address'])
+                    partition = client.getPartition(task.job_id,split_id,task.partition_id)
+                    if partition != None:
+                        task.partitions[split_id] = partition
+                    else:
+                        print "Get partition from worker %s failed" %worker['address']
+            # Get all inputs
+            if len(task.partitions) == task.num_mappers:
+                break
+            gevent.sleep(5)
 
     def reducer(self, reduce_task):
         task = reduce_task
-        while True:
-            for (k,v) in task.splits:
-                client = zerorpc.Client()
-                k
-
-        for i in range(len(job_for_reduces)):
-            reducer = self.create_reduce_instance(self.class_name)
+        # No this job, create it
+        if self.all_reduce_task_list.has_key(task.job_id) == False:
+            reduce_list = {}
+            reduce_list[task.partition_id]=task
+            self.all_reduce_task_list[task.job_id] = reduce_list
+        else:
+            self.all_reduce_task_list[task.job_id][task.partition_id] = task
+        if task.className == 'WordCount':
+            reducer = mr_classes.WordCountReduce()
+        if task.className == 'Sort':
+            reducer = mr_classes.SortReduce()
+        if task.className == 'hammingEnc':
+            reducer = mr_classes.hammingEncReduce()
+        if task.className == 'hammingDec':
+            reducer = mr_classes.hammingDecReduce()
+        if task.className == 'hammingFix':
+            reducer = mr_classes.hammingFixReduce()
+        job_for_reduces = self.collectAllInputsForReducer(task)
+        reducer.set_output_oder((job_for_reduces.keys()[0])%10) # todo change (job_for_reduces.keys()[0]) to 编号
+        for i in job_for_reduces.keys():
             keys = job_for_reduces[i].keys()
             keys.sort()
             for k in keys:
-                reducer.reduce(k, job_for_reduces[i][k])
-            reducer.write_result(self.output_base,i)
-        print('Explicit context to reducer')
-        gevent.sleep(0)
-        print('Implicit context switch back to reducer')
+                reducer.reduce(i,k,job_for_reduces.get(i)[k])
+        reducer.write_Jason_result(self.output_base)
+        reducer.write_txt_result(self.output_base)
 
 
     def getMyAddress(self):
@@ -86,6 +128,13 @@ class Worker():
             return addr + ":" + port
         except socket.error:
             return "127.0.0.1"
+
+
+    def getPartition(self,job_id,split_id,partition_id):
+        if self.all_map_task_list.has_key(job_id):
+            if self.all_map_task_list[job_id].has_key(split_id):
+                return self.all_map_task_list[job_id][split_id][partition_id]
+        return None
 
 
     def startRPCServer(self):
@@ -105,8 +154,10 @@ class Worker():
         client.connect(self.master_address)
 
 
-    def startMap(self, task):
-        gevent.spawn(self.mapper(task))
+    def startMap(self,task):
+        thread = gevent.spawn(self.mapper(task))
+        if(thread):
+            return 0
 
 
     def startReduce(self, task):
@@ -116,7 +167,9 @@ class Worker():
     def run(self):
         self.register()
         self.startRPCServer()
-        gevent.joinall([
-            gevent.spawn(self.mapper),
-            gevent.spawn(self.reducer),
-        ])
+
+if __name__ == '__main__':
+    master_address = sys.argv[1]
+    worker_address = sys.argv[2]
+    worker = Worker(master_address, worker_address)
+    worker.run()
