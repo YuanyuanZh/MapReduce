9502e407bedd8ffb60282d540d31a3371622bab8
Yuanyuan Zhang <yzhang171@dons.usfca.edu>
2015-10-21 19:49:22 -0700

    modify bash

--- testmr_collect.sh
+++ testmr_collect.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "Testing mr_collect.py"
-python mr_collect.py wordcount_out WordCount_result
-python mr_collect.py sort_out Sort_result
-python mr_collect.py hammingenc_out hammingEnc_result
-python mr_collect.py hammingfix_out hammingFix_result
-python mr_collect.py hammingchk_out hammingChk_result
-python mr_collect.py hammingdec_out hammingDec_result
-python mr_collect.py hammingerr_out hammingErr_result
+python mr_collect.py wordcount_out WordCount_collectresult
+python mr_collect.py sort_out Sort_collectresult
+python mr_collect.py hammingenc_out hammingEnc_collectresult
+python mr_collect.py hammingfix_out hammingFix_collectresult
+python mr_collect.py hammingchk_out hammingChk_collectresult
+python mr_collect.py hammingdec_out hammingDec_collectresult
+python mr_collect.py hammingerr_out hammingErr_collectresult
 
 echo "Collect job finish"
